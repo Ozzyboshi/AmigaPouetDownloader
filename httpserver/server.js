928dd6b66fb6e6ef77b93155577d9c608267ac5d
Ozzyboshi <gun101@email.it>
2026-05-30 11:50:36 +0200

    accept outdated https certs

--- httpserver/server.js
+++ httpserver/server.js
@@ -7,8 +7,6 @@ http.createServer(function (req, resp)
   var h = req.headers;
   h['user-agent'] = "Mozilla/6.0"
   
-  //h.host = "2go.ch/dist/spreadpoint-talking_heads.zip";
-  //h.host = "2go.ch";
   downloadurl=req.url.substring(1);
   downloadpath=url.parse(downloadurl).pathname;
   downloadhost=url.parse(downloadurl).host;
@@ -38,11 +36,16 @@ http.createServer(function (req, resp)
         console.log("New Request url:"+downloadurl);
         console.log("New Download path:"+downloadpath);
         resp.writeHead(resp2.statusCode, resp2.headers);
-        var req3 = https.request({ host: h.host, port: 443, path: downloadpath, method: req.method, headers: h }, function (resp3) 
+        var req3 = https.request({ host: h.host, port: 443, path: downloadpath, method: req.method, headers: h, rejectUnauthorized: false }, function (resp3) 
         {
           resp.writeHead(resp3.statusCode, resp3.headers);
           resp3.on('data', function (d) { resp.write(d); });
           resp3.on('end', function () { resp.end(); });
+        });
+        req3.on('error', function (err) {
+          console.error("HTTPS req3 error (http->redirect): " + err.message);
+          if (!resp.headersSent) resp.writeHead(502);
+          resp.end("Proxy error: " + err.message);
         });
         resp2.on('data', function (d) { req3.write(d); });
         resp2.on('end', function () { req3.end(); });
@@ -54,6 +57,11 @@ http.createServer(function (req, resp)
         resp2.on('end', function () { resp.end(); });
       }
     });
+    req2.on('error', function (err) {
+      console.error("HTTP req2 error: " + err.message);
+      if (!resp.headersSent) resp.writeHead(502);
+      resp.end("Proxy error: " + err.message);
+    });
     req.on('data', function (d) { req2.write(d); });
     req.on('end', function () { req2.end(); });
   }
@@ -61,7 +69,7 @@ http.createServer(function (req, resp)
   {
     console.log("Downloading from https server");
     h.host=downloadhost;
-    var req2 = https.request({ host: h.host, port: 443, path: downloadpath, method: req.method, headers: h }, function (resp2) 
+    var req2 = https.request({ host: h.host, port: 443, path: downloadpath, method: req.method, headers: h, rejectUnauthorized: false }, function (resp2) 
     {
       if (resp2.statusCode == "302" || resp2.statusCode == "301" || resp2.statusCode == "308" )
       {
@@ -74,11 +82,16 @@ http.createServer(function (req, resp)
         console.log("New Request url:"+downloadurl);
         console.log("New Download path:"+downloadpath);
         resp.writeHead(resp2.statusCode, resp2.headers);
-        var req3 = https.request({ host: h.host, port: 443, path: downloadpath, method: req.method, headers: h }, function (resp3) 
+        var req3 = https.request({ host: h.host, port: 443, path: downloadpath, method: req.method, headers: h, rejectUnauthorized: false }, function (resp3) 
         {
           resp.writeHead(resp3.statusCode, resp3.headers);
           resp3.on('data', function (d) { resp.write(d); });
           resp3.on('end', function () { resp.end(); });
+        });
+        req3.on('error', function (err) {
+          console.error("HTTPS req3 error (https->redirect): " + err.message);
+          if (!resp.headersSent) resp.writeHead(502);
+          resp.end("Proxy error: " + err.message);
         });
         resp2.on('data', function (d) { req3.write(d); });
         resp2.on('end', function () { req3.end(); });
@@ -90,8 +103,12 @@ http.createServer(function (req, resp)
         resp2.on('end', function () { resp.end(); });
       }
     });
+    req2.on('error', function (err) {
+      console.error("HTTPS req2 error: " + err.message);
+      if (!resp.headersSent) resp.writeHead(502);
+      resp.end("Proxy error: " + err.message);
+    });
     req.on('data', function (d) { req2.write(d); });
     req.on('end', function () { req2.end(); });
   }
 }).listen(9999, "0.0.0.0");
-
